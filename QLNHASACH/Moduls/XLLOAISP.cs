9a86e7bb08b154c52ecdbe01dbdef61551aedcc3
Henry <Henry@DESKTOP-RD7GGH3>
2020-06-18 14:59:55 +0700

    Hoan thanh class XL

---
+++ QLNHASACH/Moduls/XLLOAISP.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNHASACH.Moduls
+{
+    class XLLOAISP: XLBANG
+    {
+        public XLLOAISP() : base("LOAISP") { }
+        public XLLOAISP(string pQuery) : base("LOAISP", pQuery) { }
+    }
+}
