83a845a77a5f2f382ec7c644f57d5a550169a5a9
Henry <Henry@DESKTOP-RD7GGH3>
2020-06-18 13:06:59 +0700

    Add project files.

---
+++ QLNHASACH/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace QLNHASACH
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tendangnhap = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tendangnhap
+            // 
+            this.tendangnhap.AutoSize = true;
+            this.tendangnhap.Location = new System.Drawing.Point(45, 82);
+            this.tendangnhap.Name = "tendangnhap";
+            this.tendangnhap.Size = new System.Drawing.Size(116, 19);
+            this.tendangnhap.TabIndex = 0;
+            this.tendangnhap.Text = "Tên đăng nhập";
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 396);
+            this.Controls.Add(this.tendangnhap);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmDangNhap";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tendangnhap;
+    }
+}
+
