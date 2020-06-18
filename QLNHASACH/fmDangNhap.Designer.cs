7cd1f911efb6b7c7e371390de852a38626df64a4
Nguyễn Thị Quỳnh <nguyenthiquynh30032000@outlook.com>
2020-06-18 14:14:47 +0700

    fmDoiMatKhau

---
+++ QLNHASACH/fmDangNhap.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace QLNHASACH
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDangNhap));
+            this.tendangnhap = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // tendangnhap
+            // 
+            this.tendangnhap.AutoSize = true;
+            this.tendangnhap.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.tendangnhap.Location = new System.Drawing.Point(63, 43);
+            this.tendangnhap.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.tendangnhap.Name = "tendangnhap";
+            this.tendangnhap.Size = new System.Drawing.Size(93, 13);
+            this.tendangnhap.TabIndex = 0;
+            this.tendangnhap.Text = "Tên đăng nhập:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(63, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mật khẩu:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 43);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 80);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(81, 132);
+            this.simpleButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(98, 38);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Đăng nhập";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.simpleButton2.Location = new System.Drawing.Point(198, 132);
+            this.simpleButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(87, 38);
+            this.simpleButton2.TabIndex = 2;
+            this.simpleButton2.Text = "Thoát";
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 206);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tendangnhap);
+            this.Name = "frmDangNhap";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tendangnhap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+    }
+}
+
--- QLNHASACH/fmDoiMatKhau.Designer.cs
+++ QLNHASACH/fmDoiMatKhau.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QLNHASACH
 {
-    partial class frmDangNhap
+    partial class fmDoiMatKhau
     {
         /// <summary>
         /// Required designer variable.
@@ -28,81 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDangNhap));
-            this.tendangnhap = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmDoiMatKhau));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
             this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
             this.SuspendLayout();
             // 
-            // tendangnhap
-            // 
-            this.tendangnhap.AutoSize = true;
-            this.tendangnhap.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
-            this.tendangnhap.Location = new System.Drawing.Point(45, 82);
-            this.tendangnhap.Name = "tendangnhap";
-            this.tendangnhap.Size = new System.Drawing.Size(136, 19);
-            this.tendangnhap.TabIndex = 0;
-            this.tendangnhap.Text = "Tên đăng nhập:";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(45, 150);
+            this.label1.Location = new System.Drawing.Point(48, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.Size = new System.Drawing.Size(102, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Mật khẩu:";
+            this.label1.Text = "Nhập mật khẩu mới:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(204, 82);
+            this.textBox1.Location = new System.Drawing.Point(184, 46);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(198, 27);
+            this.textBox1.Size = new System.Drawing.Size(134, 21);
             this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nhập lại mật khẩu:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(204, 147);
+            this.textBox2.Location = new System.Drawing.Point(184, 82);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(198, 27);
+            this.textBox2.Size = new System.Drawing.Size(134, 21);
             this.textBox2.TabIndex = 1;
             // 
             // simpleButton1
             // 
             this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
-            this.simpleButton1.Location = new System.Drawing.Point(77, 230);
+            this.simpleButton1.Location = new System.Drawing.Point(84, 120);
             this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(132, 44);
+            this.simpleButton1.Size = new System.Drawing.Size(93, 41);
             this.simpleButton1.TabIndex = 2;
-            this.simpleButton1.Text = "Đăng nhập";
+            this.simpleButton1.Text = "Cập Nhật";
             // 
             // simpleButton2
             // 
             this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
-            this.simpleButton2.Location = new System.Drawing.Point(337, 230);
+            this.simpleButton2.Location = new System.Drawing.Point(198, 120);
             this.simpleButton2.Name = "simpleButton2";
-            this.simpleButton2.Size = new System.Drawing.Size(122, 44);
+            this.simpleButton2.Size = new System.Drawing.Size(93, 41);
             this.simpleButton2.TabIndex = 2;
-            this.simpleButton2.Text = "Thoát";
+            this.simpleButton2.Text = "Không";
             // 
-            // frmDangNhap
+            // fmDoiMatKhau
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 332);
+            this.ClientSize = new System.Drawing.Size(361, 200);
             this.Controls.Add(this.simpleButton2);
             this.Controls.Add(this.simpleButton1);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.tendangnhap);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "frmDangNhap";
-            this.Text = "Đăng nhập";
+            this.Name = "fmDoiMatKhau";
+            this.Text = "fmDoiMatKhau";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,12 +107,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label tendangnhap;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
         private DevExpress.XtraEditors.SimpleButton simpleButton2;
     }
 }
-
